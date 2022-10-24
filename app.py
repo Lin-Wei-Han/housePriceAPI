f9507eaf642498c6e314e47fd99a849d3aaf27c2
Lin-Wei-Han <judy97678@gmail.com>
2022-10-24 20:39:36 +0800

    @change procfile

--- app.py
+++ app.py
@@ -6,17 +6,20 @@ import pandas as pd
 
 app = FastAPI()
 
-class HousePriceItem(BaseModel):
-    buildingArea:float
-    roomAmount:int
-    livingroomAmount:int
-    bathroomAmount:int
 
-with gzip.open('randomForest_houseprice.pgz', 'rb') as f:
+class HousePriceItem(BaseModel):
+    buildingArea: float
+    roomAmount: int
+    livingroomAmount: int
+    bathroomAmount: int
+
+
+with gzip.open('randomForest_houseprice.pkl', 'rb') as f:
     housePriceModel = pickle.load(f)
 
+
 @app.post('/getHousePrice')
-async def housePrice_endpoint(item:HousePriceItem):
+async def housePrice_endpoint(item: HousePriceItem):
     df = pd.DataFrame([item.dict().values()], columns=item.dict().keys())
     result = housePriceModel.predict(df)
-    return {"prediction":int(result)}
+    return {"prediction": int(result)}
